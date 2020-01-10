60403594eaad6d086addb9217cba8538929ab6b8
KirichenkoOleg <strannik.kirichenko136@gmail.com>
2020-01-10 23:15:45 +0200

    added processing js, sass/scss, minification images, fonts, page live reloading

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-//import $ from 'jquery'; //импорт модуля-ей
+import $ from 'jquery'; //импорт модуля-ей
 //подкл. библиотеку устан. локально через консоль (npm i jquery --save)
 //таким жэ образом можно подкл. любую другую библиотеку
+import 'bootstrap';
+//import 'popper.js';//нужен для работы js bootstrap
+
 import avg from './some.js';
 
-// $('.title').html('Some text la la la');
+$(function () {
 
-console.log( avg(1, 3, 4) );
+  // $('.title').html('Some text la la la');
+
+  console.log(avg(1, 3, 4));
+
+});
+
 
 import './scss/main.scss';
+// import icon_m from './img/header/icon-m.png';
+// import icon_l from './img/header/icon-l.png';
+
+// import icon_m from './img/*/**.png';
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
 
 const path = require('path'); //установили модуль (path), затем импортируем он устанавливает привильные пути
+const HtmlWebpackPlugin = require('html-webpack-plugin');//обработка html файла
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-//const CopyPlugin = require('copy-webpack-plugin');//Копирует отдельные файлы или целые каталоги, которые уже существуют, в каталог сборки.
+const CopyPlugin = require('copy-webpack-plugin');//Копирует отдельные файлы или целые каталоги, которые уже существуют, в каталог сборки.
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');// перед сборкой очищает конечную папку
+
+// const PATH = {
+//     //более простой доступ к путям с помощю глобальных переменных
+//     src: path.join(__dirname, './src'),
+//     dist: path.join(__dirname, './dist'),
+//     accets: 'accets/',
+// };
 
 let conf = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, './dist'),//изм. итоговую папку
+        path: path.resolve(__dirname, 'dist'),//изм. итоговую папку
         filename: 'js/main.js',//изменить итоговое имя файла
-        publicPath: 'dist/' //относительная ссылка на js файл для браузера /livereload/
+        //publicPath: 'dist/', //относительная ссылка на js файл для браузера /livereload/
+        publicPath: '/' //в случае когда html перенесен в src/index.html
     },
     devServer: {
-        overlay: true, //выводит сообщение об ошибке
+        //contentBase: './dist',//в случае когда html перенесен в src/index.html и отвечает за то где будет открываться webpack
+        contentBase: path.join(__dirname, './dist'),// равнозначное решение как и на пред. строке
+        overlay: true,
     },
     module: {
         rules: [
@@ -22,7 +33,7 @@ let conf = {
             {
                 test: /\.js$/, 
                 loader: 'babel-loader',
-                // exclude: '/node_modules/'
+                //exclude: '/node_modules/', //что-то, что не должно обрабатываться
             },
             {
                 test: /\.(sass|scss)$/,
@@ -30,7 +41,7 @@ let conf = {
                     {
                         loader: MiniCssExtractPlugin.loader,
                         options: {
-                            publicPath: '../',
+                            publicPath: '../', //влияет на путь к картинкам в файлах стилей /это значение оставляет исходный путь/
                             hmr: process.env.NODE_ENV === 'development',
                         }
                     },
@@ -39,7 +50,10 @@ let conf = {
                 ]
             },
             {
-                test: /\.(png|jpe?g|gif|svg)$/,
+                test: /\.(png|svg|jpg|gif)$/,
+                // include: [
+                    // path.resolve(__dirname, './src/img/'),
+                // ],
                 use: [
                     {
                         loader: 'file-loader',
@@ -60,17 +74,30 @@ let conf = {
                     }
                 ],
             },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[folder]/[name].[ext]',// сохранит вложенность
+                    outputPath: 'fonts/',
+                }
+            }
         ]
     },
     plugins: [
         new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html',//откуда брать шаблон html
+            filename: 'index.html',
+            inject: false, //говорит Webpack, что не нужно встраивать ссылки на js и css файл в HTML код самостоятельно: мы сделаем всё сами вручную
+        }),
+        // new CopyPlugin([
+            // { from: 'src/index.html', to: './' },
+        // ]),
         new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // all options are optional
             filename: 'css/style.css',
-            chunkFilename: '[id].css',
             ignoreOrder: false, // Enable to remove warnings about conflicting order
-        })
+        }),
     ]
 };
 // module.exports = conf;
@@ -78,7 +105,6 @@ let conf = {
 //------------настраиваем режим работы webpack
 module.exports = (env, options) => {
     let mode = options.mode === 'production';// проверяем режим зборки по свойству объекта /conf/
-    // console.log( mode );
     // conf.devtool = mode ? 'source-map' : 'eval-sourcemap';//будет доступен source-map
     //для девелоп режима все в один файл, для продакшен режима карта кода вынесется в отдельный файл
     
@@ -86,6 +112,3 @@ module.exports = (env, options) => {
 
     return conf;
 };
-
-
-// ----------------------------------------------------------------------------1:32:00
